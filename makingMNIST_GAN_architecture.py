568f3be3ce0d89a09f77c610b928c4c4da70c3fd
FoxtrotUniformCharlieKiloUniform <46904044+FoxtrotUniformCharlieKiloUniform@users.noreply.github.com>
2024-08-12 08:16:48 -0400

    Update makingMNIST_GAN_architecture.py
    Most recent commit acting as a backup
    working but for some reason Generator output is: 
    (300, 1, 28, 28) which sux. And the outputs all look really similar relative to one another.

--- makingMNIST_GAN_architecture.py
+++ makingMNIST_GAN_architecture.py
@@ -18,7 +18,7 @@ torch.manual_seed(123)
 np.random.seed(123)
 
 #model parameters
-batch_size = 32
+batch_size = 300 #60000 should be divisible by this number
 num_epochs = 450
 
 
@@ -63,6 +63,8 @@ class MnistDataloader1(object):
         
         return (x_train_tensor, y_train_tensor), (x_test_tensor, y_test_tensor)
     
+
+#TODO make the first dataloader object do9 this as well by changing load_data to __getitem__ and returning [index] of each one
 class MnistDataLoaderTrain(object):
     def __init__(self, x_train_tensor, y_train_tensor):        
         self.x_train_tensor = x_train_tensor
@@ -72,18 +74,9 @@ class MnistDataLoaderTrain(object):
         return x_train_tensor.shape[0]
 
     def __getitem__(self, index):
+        #print(self.y_train_tensor[index])
         return (self.x_train_tensor[index], self.y_train_tensor[index])
 
-class MnistDataLoaderTest(object):
-    def __init__(self, x_test_tensor, y_test_tensor):        
-        self.x_train_tensor = x_test_tensor
-        self.y_train_tensor = y_test_tensor
-
-    def __len__(self):
-        return x_test_tensor.shape[0]
-
-    def __getitem__(self, index):
-        return (self.x_test_tensor[index], self.y_test_tensor[index])
     
 # Set file paths based on added MNIST Datasets
 input_path = r'C:\Users\Matt\Documents\Pytorch_ML\Generative\GAN\Data\archive'
@@ -99,10 +92,7 @@ mnist_dataloader = MnistDataloader1(training_images_filepath, training_labels_fi
 dataLoaderTrain = MnistDataLoaderTrain(x_train_tensor, y_train_tensor)
 
 #TODO: consolidate train and test dataloaders into one? for both this and random numbers?
-#dataLoaderTest = DataLoader(x_test_tensor, y_train_tensor)
-
-
-mnistDataSetTrain = DataLoader(dataLoaderTrain, batch_size = batch_size, shuffle = True)
+mnistDataSetTrain = DataLoader(dataLoaderTrain, batch_size = batch_size, shuffle = False)
 
 
 
@@ -125,9 +115,6 @@ def show_images(images, title_texts):
     plt.pause(3.5)
     plt.close("all")  # Close the figure after showing
 
-    
-
-
 # Show some random training and test images
 images_2_show = []
 titles_2_show = []
@@ -149,57 +136,103 @@ for i in range(0, 5):
 
 #_______________________________________________________ model time ________________________________________________________________________________________
 
-
-
-
 img_size = x_train_tensor[0].size(0)
 print("Image size is", img_size)
 print("Entire tensor size is ",x_train_tensor.size())
 
 class Generator(nn.Module):
-    def __init__(self, input_length,batch_size):
-        super(Generator,self).__init__()
+    def __init__(self, input_length, batch_size):
+        super(Generator, self).__init__()
         
         self.batch_size = batch_size
-        
         self.input_length = input_length
-        self.Fc1 = nn.Linear(input_length**2*batch_size, 510)
-        self.Fc2 = nn.Linear(510, input_length**2*batch_size)
-        #reshape output to be 28 x 28
-    def forward(self,x):
-        input_length = self.input_length
-        batch_size = self.batch_size
+        
+        # Encoding layers (downsampling)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1)
 
-        #x = torch.flatten(x).unsqueeze(-1)
-        x = torch.flatten(x)
-        x = F.relu(self.Fc1(x))
-        x = F.sigmoid(self.Fc2(x))
-        x =  torch.reshape(x, (batch_size, input_length, input_length))
+        # Decoding layers (upsampling)
+        self.deconv1 = nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
+        self.deconv3 = nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1, dilation = 2, output_padding = 1)
 
-        outputs = x
-        return outputs
-    
+        # Batch normalization layers
+        self.bn1 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.bn5 = nn.BatchNorm2d(64)
+
+    def forward(self, x):
+        # Reshape the input to add the channel dimension (batch_size, 1, input_length, input_length)
+        x = x.view(-1, 1, self.input_length, self.input_length)
+        
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+
+        #print(f"Level 4 x shape {x.shape}")
+        # Decoding path with transposed convolutional layers, batch normalization, and ReLU activation
+        x = F.relu(self.bn4(self.deconv1(x)))
+        
+        #print(f"Level 5 x shape {x.shape}")
+        x = F.relu(self.bn5(self.deconv2(x)))
+
+        
+        #print(f"Level 6 x shape {x.shape}")
+
+        # Final output layer with tanh activation
+        x = torch.tanh(self.deconv3(x))
+        
+        print(f"level 7 x shape {x.shape}")
+        
+        # Remove the channel dimension to return to the original input shape
+
+        x = x.view(batch_size, self.input_length, self.input_length)
+        
+        return x
     
 class Discriminator(nn.Module):
-        def __init__(self, input_length, batch_size):
-            super(Discriminator,self).__init__()
-            
-            self.batch_size = batch_size
-            self.input_length = input_length
-            self.Fc1 = nn.Linear(input_length**2 *batch_size, 256)
-            self.Fc2 = nn.Linear(256, batch_size)
-
-        def forward(self,x):
-            input_length = self.input_length
-            batch_size = self.batch_size
-
-            x = torch.flatten(x)
-            x = F.relu(self.Fc1(x))
-            x = F.sigmoid(self.Fc2(x))
-
-            outputs = x
-            return outputs
+    def __init__(self, input_length, batch_size):
+        super(Discriminator, self).__init__()
         
+        self.batch_size = batch_size
+        self.input_length = input_length
+        
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(batch_size, 64, kernel_size=5, stride=2, padding=2)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
+        
+        # Fully connected layers
+        self.fc1 = nn.Linear(64**2, 256)
+        self.fc2 = nn.Linear(256, batch_size)
+        
+        # Dropout for regularization
+        self.dropout = nn.Dropout(0.3)
+
+    def forward(self, x):
+        # Reshape the input to match convolutional input size
+        x = x.view(-1, self.input_length, self.input_length)
+
+        # Convolutional layers with ReLU activation
+        x = F.leaky_relu(self.conv1(x))
+        x = F.leaky_relu(self.conv2(x))
+        x = F.leaky_relu(self.conv3(x))
+        
+        # Flatten the output from the convolutional layers
+        x = torch.flatten(x)
+        
+        # Fully connected layers with ReLU and Dropout
+        x = F.leaky_relu(self.fc1(x))
+        x = self.dropout(x)
+        
+        # Output layer with sigmoid activation
+        x = torch.sigmoid(self.fc2(x))
+        
+        return x
+
         
 def Train(epochs, verbose, showImages):
     generator = Generator(img_size, batch_size).to(device)
@@ -223,9 +256,9 @@ def Train(epochs, verbose, showImages):
             true_data = x_train_tensor.to(device)
             true_labels = y_train_tensor.to(device)
             
-
             gen_discrim_out = discriminator(data).to(device)
 
+            
             # Calculate the generator loss
             generator_loss = loss(gen_discrim_out, true_labels)
             generator_loss.backward()
@@ -252,7 +285,8 @@ def Train(epochs, verbose, showImages):
         
         if showImages == True:
             title = f"Epoch [{i+1}/{epochs}] generated image"
-            show_images(data, title)
+                #just doing 12 random images for now
+            show_images(data[0:10], title)
 
     if saveModel == True:
         return noise, generator, discriminator, saveModel
